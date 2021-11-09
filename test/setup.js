cf4a8992e7ec0cef2b1d689b1e7d803a6cecb6a6
yasudanaoya <tamago.708@gmail.com>
2021-11-10 02:54:45 +0900

    feat(docs): add reference source

--- test/setup.js
+++ test/setup.js
@@ -2,5 +2,6 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import VueCompositionApi from '@vue/composition-api'
 
+// docs: https://vuetifyjs.com/en/getting-started/unit-testing/#bootstrapping-vuetify
 Vue.use(Vuetify)
 Vue.use(VueCompositionApi)
