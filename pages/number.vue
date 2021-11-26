92b9ff888dc2ab513acae4e401c3228acb5d9153
tatsuya yokokura <yokokura.tatsuya@gmail.com>
2021-11-26 21:34:37 +0900

    yokokura ver1

--- pages/number.vue
+++ pages/number.vue
@@ -25,7 +25,7 @@
 
 <script lang="ts">
 import { defineComponent, ref } from '@vue/composition-api'
-import { Big } from 'big.js'
+// import { Big } from 'big.js'
 import InputNumber from '~/components/atoms/InputNumber.vue'
 
 export default defineComponent({
@@ -42,17 +42,46 @@ export default defineComponent({
     // 10になる答えを入れる配列
     const results = ref<Array<String>>([])
 
+    const resultUntil2Digit = ref<number>(4)
+
+    // 3番目の値を四則計算する関数
+    const calcUntil3 = (resultUntil2Digit: number, third: number, fourth: number) => {
+      if (calcUntil4((resultUntil2Digit + third), fourth)) return true
+      if (calcUntil4((resultUntil2Digit - third), fourth)) return true
+      if (calcUntil4((resultUntil2Digit * third), fourth)) return true
+      if (calcUntil4((resultUntil2Digit / third), fourth)) return true
+    }
+
+    // 4番目の値を四則計算する関数
+    const calcUntil4 = (resultUntil3Digit: number, fourth: number) => {
+      const goalNum = 10
+      if (resultUntil3Digit + fourth === goalNum) {
+        console.log('happy1!')
+      } else if (resultUntil3Digit - fourth === goalNum) {
+        console.log('happy2!')
+      } else if (resultUntil3Digit * fourth === goalNum) {
+        console.log('happy3!')
+      } else if (resultUntil3Digit / fourth === goalNum) {
+        console.log('happy4!')
+      } else {
+        console.log('false!!!')
+        return false
+      }
+    }
+
     // "計算実行"ボタン押下時に実行されるメソッド
     // ここでresultsに10になる答えを入れる
     // 小数点の計算などでバグる可能性あるので、big.jsをインストール済です
     // https://github.com/MikeMcl/big.js/
     const execute = () => {
-      results.value = []
-      console.log('execute')
-      const f: Big = Big(first.value)
-      const s: Big = Big(second.value)
-      const t: Big = Big(third.value)
-      results.value.push(String(f.div(s).times(t).toFixed(0)))
+      // results.value = []
+      // const f: Big = Big(first.value)
+      // const s: Big = Big(second.value)
+      // const t: Big = Big(third.value)
+      // console.log(f.div(s).times(s).toFixed(0))
+      // results.value.push(String(f.div(s).times(t).toFixed(0)))
+
+      calcUntil3(resultUntil2Digit.value, third.value, fourth.value)
     }
 
     return {
