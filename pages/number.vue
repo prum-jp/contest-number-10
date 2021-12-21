533354a29b8e3978d7fae70b11a44c1237524296
tatsuya yokokura <yokokura0413@gmail.com>
2021-12-21 17:49:30 +0900

    関数のリファクタ

--- pages/number.vue
+++ pages/number.vue
@@ -37,64 +37,93 @@ export default defineComponent({
     const second = ref<number>(0)
     const third = ref<number>(0)
 
-    const goalNum: number = 10
+    // 計算終了時に算出される値
+    const resultNum: number = 10
+
     // 10になる答えを入れる配列
-    const results = reactive<any[]>([])
+    const results = reactive<string[]>([])
     // "計算実行"ボタン押下時に実行されるメソッド
     // ここでresultsに10になる答えを入れる
     // 小数点の計算などでバグる可能性あるので、big.jsをインストール済です
     // https://github.com/MikeMcl/big.js/
 
-    const calcSecondaryGaolNumByPlus = (
+    // 2つの数値を与えた時に+の計算で期待する値にする計算式を求める関数
+    const calcWishNumByPlus = (
       second: number,
       third: number,
-      secondaryGaolNum: number
+      wishNum: number
     ): string | boolean => {
-      if (second + third === secondaryGaolNum) {
+      if (second + third === wishNum) {
         return `(${second} + ${third})`
       } else {
         return false
       }
     }
 
-    const calcSecondaryGaolNumByMinus = (
+    // 2つの数値を与えた時に-の計算で期待する値にする計算式を求める関数
+    const calcWishNumByMinus = (
       second: number,
       third: number,
-      secondaryGaolNum: number
+      wishNum: number
     ): string | boolean => {
-      if (second - third === secondaryGaolNum) {
+      if (second - third === wishNum) {
         return `(${second} - ${third})`
-      } else if (third - second === secondaryGaolNum) {
+      } else if (third - second === wishNum) {
         return `(${third} - ${second})`
       } else {
         return false
       }
     }
 
-    const calcSecondaryGaolNumByTimes = (
+    // 2つの数値を与えた時に×の計算で期待する値にする計算式を求める関数
+    const calcWishNumByTimes = (
       second: number,
       third: number,
-      secondaryGaolNum: number
+      wishNum: number
     ): string | boolean => {
-      if (second * third === secondaryGaolNum) {
+      if (second * third === wishNum) {
         return `(${second} × ${third})`
       } else {
         return false
       }
     }
 
-    const calcSecondaryGaolNumByDivide = (
+    // 2つの数値を与えた時に÷の計算で期待する値にする計算式を求める関数
+    const calcWishNumByDivide = (
       second: number,
       third: number,
-      secondaryGaolNum: number
+      wishNum: number
     ): string | boolean => {
-      if (second / third === secondaryGaolNum) {
+      if (second / third === wishNum) {
         return `(${second} ÷ ${third})`
-      } else if (third / second === secondaryGaolNum) {
+      } else if (third / second === wishNum) {
         return `(${third} ÷ ${second})`
       } else {
         return false
       }
+    }
+
+    // 2つの数値を与えた時に期待する値にするための計算式を配列にまとめるための関数
+    const getSucceedFormula = (
+      second: number,
+      third: number,
+      wishNum: number
+    ): Array<any> => {
+      const resultArray = []
+
+      const resultByPlus = calcWishNumByPlus(second, third, wishNum)
+      if (resultByPlus) resultArray.push(resultByPlus)
+
+      const resultByMinus = calcWishNumByMinus(second, third, wishNum)
+      if (resultByMinus) resultArray.push(resultByMinus)
+
+      const resultByTimes = calcWishNumByTimes(second, third, wishNum)
+      if (resultByTimes) resultArray.push(resultByTimes)
+
+      const resultByDivide = calcWishNumByMinus(second, third, wishNum)
+      if (resultByDivide) resultArray.push(resultByDivide)
+
+      return resultArray
     }
 
     const calc10ByPlus = (
@@ -102,38 +131,10 @@ export default defineComponent({
       second: number,
       third: number
     ): void => {
-      const secondaryGaolNum: number = goalNum - first
-      const secondaryResultArray = []
+      const wishNum: number = resultNum - first
+      const resultArray = getSucceedFormula(second, third, wishNum)
 
-      const resultByPlus = calcSecondaryGaolNumByPlus(
-        second,
-        third,
-        secondaryGaolNum
-      )
-      if (resultByPlus) secondaryResultArray.push(resultByPlus)
-
-      const resultByMinus = calcSecondaryGaolNumByMinus(
-        second,
-        third,
-        secondaryGaolNum
-      )
-      if (resultByMinus) secondaryResultArray.push(resultByMinus)
-
-      const resultByTimes = calcSecondaryGaolNumByTimes(
-        second,
-        third,
-        secondaryGaolNum
-      )
-      if (resultByTimes) secondaryResultArray.push(resultByTimes)
-
-      const resultByDivide = calcSecondaryGaolNumByMinus(
-        second,
-        third,
-        secondaryGaolNum
-      )
-      if (resultByDivide) secondaryResultArray.push(resultByDivide)
-
-      secondaryResultArray.forEach((result) => {
+      resultArray.forEach((result) => {
         results.push(`${result} + ${first}`)
       })
     }
@@ -143,38 +144,10 @@ export default defineComponent({
       second: number,
       third: number
     ): void => {
-      const secondaryGaolNum: number = goalNum + first
-      const secondaryResultArray = []
+      const wishNum: number = resultNum + first
+      const resultArray = getSucceedFormula(second, third, wishNum)
 
-      const resultByPlus = calcSecondaryGaolNumByPlus(
-        second,
-        third,
-        secondaryGaolNum
-      )
-      if (resultByPlus) secondaryResultArray.push(resultByPlus)
-
-      const resultByMinus = calcSecondaryGaolNumByMinus(
-        second,
-        third,
-        secondaryGaolNum
-      )
-      if (resultByMinus) secondaryResultArray.push(resultByMinus)
-
-      const resultByTimes = calcSecondaryGaolNumByTimes(
-        second,
-        third,
-        secondaryGaolNum
-      )
-      if (resultByTimes) secondaryResultArray.push(resultByTimes)
-
-      const resultByDivide = calcSecondaryGaolNumByMinus(
-        second,
-        third,
-        secondaryGaolNum
-      )
-      if (resultByDivide) secondaryResultArray.push(resultByDivide)
-
-      secondaryResultArray.forEach((result) => {
+      resultArray.forEach((result) => {
         results.push(`${result} - ${first}`)
       })
     }
@@ -184,38 +157,10 @@ export default defineComponent({
       second: number,
       third: number
     ): void => {
-      const secondaryGaolNum: number = goalNum / first
-      const secondaryResultArray = []
+      const wishNum: number = resultNum / first
+      const resultArray = getSucceedFormula(second, third, wishNum)
 
-      const resultByPlus = calcSecondaryGaolNumByPlus(
-        second,
-        third,
-        secondaryGaolNum
-      )
-      if (resultByPlus) secondaryResultArray.push(resultByPlus)
-
-      const resultByMinus = calcSecondaryGaolNumByMinus(
-        second,
-        third,
-        secondaryGaolNum
-      )
-      if (resultByMinus) secondaryResultArray.push(resultByMinus)
-
-      const resultByTimes = calcSecondaryGaolNumByTimes(
-        second,
-        third,
-        secondaryGaolNum
-      )
-      if (resultByTimes) secondaryResultArray.push(resultByTimes)
-
-      const resultByDivide = calcSecondaryGaolNumByMinus(
-        second,
-        third,
-        secondaryGaolNum
-      )
-      if (resultByDivide) secondaryResultArray.push(resultByDivide)
-
-      secondaryResultArray.forEach((result) => {
+      resultArray.forEach((result) => {
         results.push(`${result} × ${first}`)
       })
     }
@@ -225,38 +170,10 @@ export default defineComponent({
       second: number,
       third: number
     ): void => {
-      const secondaryGaolNum: number = goalNum * first
-      const secondaryResultArray = []
+      const wishNum: number = resultNum * first
+      const resultArray = getSucceedFormula(second, third, wishNum)
 
-      const resultByPlus = calcSecondaryGaolNumByPlus(
-        second,
-        third,
-        secondaryGaolNum
-      )
-      if (resultByPlus) secondaryResultArray.push(resultByPlus)
-
-      const resultByMinus = calcSecondaryGaolNumByMinus(
-        second,
-        third,
-        secondaryGaolNum
-      )
-      if (resultByMinus) secondaryResultArray.push(resultByMinus)
-
-      const resultByTimes = calcSecondaryGaolNumByTimes(
-        second,
-        third,
-        secondaryGaolNum
-      )
-      if (resultByTimes) secondaryResultArray.push(resultByTimes)
-
-      const resultByDivide = calcSecondaryGaolNumByMinus(
-        second,
-        third,
-        secondaryGaolNum
-      )
-      if (resultByDivide) secondaryResultArray.push(resultByDivide)
-
-      secondaryResultArray.forEach((result) => {
+      resultArray.forEach((result) => {
         results.push(`${result} ÷ ${first}`)
       })
     }
@@ -274,13 +191,13 @@ export default defineComponent({
       third,
       results,
       calc10ByPlus,
-      calcSecondaryGaolNumByPlus,
+      calcWishNumByPlus,
       calc10ByMinus,
-      calcSecondaryGaolNumByMinus,
+      calcWishNumByMinus,
       calc10ByTimes,
-      calcSecondaryGaolNumByTimes,
+      calcWishNumByTimes,
       calc10ByDivide,
-      calcSecondaryGaolNumByDivide,
+      calcWishNumByDivide,
       execute,
     }
   },
