b76a59710d81c8d1ef2fb39b4fd819e032a7e962
harue.murakami <harue.murakami@prum.jp>
2021-12-21 18:55:03 +0900

    村上、数藤

--- pages/number.vue
+++ pages/number.vue
@@ -1,6 +1,9 @@
 <template lang="pug">
   v-form
     v-container
+      v-row.mb-5 下記数字が結果となる計算式を出す
+      v-row
+        input-number(@setNumber="(value) => (sum = value)")
       v-row
         input-number(@setNumber="(value) => (first = value)")
         input-number(@setNumber="(value) => (second = value)")
@@ -12,13 +15,13 @@
         x-large
         @click="execute"
       ) 計算実行
-      v-row.mb-5 以下、10になる答え表示
+      v-row.mb-5 以下、{{ sum }}になる答え表示
       v-row(
         v-for="(result, index) in results"
         :key="`result-${index}`"
       )
         v-col(
-          cols="1"
+          cols="2"
           md="3"
         ) {{ result }}
 </template>
@@ -35,6 +38,7 @@ export default defineComponent({
 
   setup() {
     // 画面入力値
+    const sum = ref<number>(0)
     const first = ref<number>(0)
     const second = ref<number>(0)
     const third = ref<number>(0)
@@ -42,20 +46,112 @@ export default defineComponent({
     // 10になる答えを入れる配列
     const results = ref<Array<String>>([])
 
+    // ループできるように関数をラップする
+    const funPlus = (x: Big, y: Big) => {
+      return x.plus(y)
+    }
+
+    const funMinus = (x: Big, y: Big) => {
+      return x.minus(y)
+    }
+
+    const funTimes = (x: Big, y: Big) => {
+      return x.times(y)
+    }
+
+    const funDiv = (x: Big, y: Big) => {
+      // Division by zero対策      
+      if(y.eq(Big(0))) return undefined
+      return x.div(y)
+    }
+
+    // 24通りの並びを作成するメソッド
+    const genArray = (numbers: Array<Big>, permutation: Array<any>) => {
+      let i, j, k, l,array1,array2,array3;
+
+      for(i = 0;i < numbers.length; i++){
+        array1 = numbers.slice(0)
+        array1.splice(i, 1);
+        for(j = 0; j < array1.length; j++) {
+          array2 = array1.slice(0)
+          array2.splice(j, 1)
+          for(k = 0; k < array2.length; k++) {
+            array3 = array2.slice(0)
+            array3.splice(k, 1)
+            for(l = 0;l < array3.length; l++) {
+              permutation.push([numbers[i]].concat([array1[j]]).concat([array2[k]]).concat([array3[l]]))
+            }
+          }
+        }
+      }
+    }
+
+    // 式を整形できるメソッド
+    const genResult = (oparate: Function) => {
+      switch(oparate) {
+        case funPlus:
+          return '+'
+        case funMinus:
+          return '-'
+        case funTimes:
+          return '*'
+        case funDiv:
+          return '/'
+      } 
+    }
+
     // "計算実行"ボタン押下時に実行されるメソッド
     // ここでresultsに10になる答えを入れる
     // 小数点の計算などでバグる可能性あるので、big.jsをインストール済です
     // https://github.com/MikeMcl/big.js/
     const execute = () => {
       results.value = []
-      console.log('execute')
+      const m: Big = Big(sum.value)
       const f: Big = Big(first.value)
       const s: Big = Big(second.value)
       const t: Big = Big(third.value)
-      results.value.push(String(f.div(s).times(t).toFixed(0)))
+      const fo: Big = Big(fourth.value)
+
+      // 今回求めるもの
+      const master = m
+
+      // 数字の順番は24通り
+      const numbers: Array<Big> = [f, s, t, fo];
+      const permutation: Array<any> = [];
+      genArray(numbers, permutation)
+
+      // 64通り計算やってみる
+      const operators = [funPlus, funMinus, funTimes, funDiv]
+      permutation.forEach(item => {
+        // itemの0の計算
+        operators.forEach(firstOperator => {
+          // itemの1の計算
+          const sum1 = firstOperator(item[0], item[1])
+          if(!sum1) return
+          operators.forEach(secondOperator => {
+            // item2と3の計算
+            const sum2 = secondOperator(sum1, item[2])
+            if(sum2 !== undefined) {
+              operators.forEach(thirdOperator => {
+                const sum3 = thirdOperator(sum2, item[3])
+                if(!sum3) return
+                if(sum3.eq(master)) {
+                  // 式を整形
+                  const result = `((${item[0]}${genResult(firstOperator)}${item[1]})${genResult(secondOperator)}${item[2]})${genResult(thirdOperator)}${item[3]}`
+                  results.value.push(result)
+                }
+              })
+            }
+          })
+        })
+      })
+      if(!results.value.length) {
+        results.value.push('結果なし')
+      }
     }
 
     return {
+      sum,
       first,
       second,
       third,
