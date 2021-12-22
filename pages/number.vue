dbde60cce5e2e6207ff3cda4626c51fd5173a5c9
PRUM-Azuma-Shonosuke <shonosuke.azuma@prum.jp>
2021-12-22 18:11:11 +0900

    azuma-nakao

--- pages/number.vue
+++ pages/number.vue
@@ -32,10 +32,10 @@ export default defineComponent({
     // 10になる答えを入れる配列
     const results = ref<Array<String>>([])
 
-    // // 導出する正解の値
+    // 導出する正解の値
     // const answerNumber = <number>(10)
 
-    // // 10になる式を格納する
+    // 10になる式を格納する
     // const formula = ref<String>('')
 
     // 四則演算記号
@@ -63,14 +63,14 @@ export default defineComponent({
       // 四則演算記号の全バリエーションを配列へ設定
       setSymbolVariations()
 
-      // // 計算を実行
-      // if(calculation(inputNumberArray.value)){
-      //   // true：正解の式が導出できた
-      //   results.value.push(String(formula + ' = ' + answerNumber + ' ...え！？' + answerNumber + 'なったかも！！'))
-      // } else {
-      //   // false：正解の式が導出できなかった
-      //   results.value.push(String(answerNumber + 'になりませんでしたぁ'))
-      // }
+      // 計算を実行
+      if(calculation(inputNumberArray.value)){
+        // true：正解の式が導出できた
+        // results.value.push(String(formula + ' = ' + answerNumber + ' ...え！？' + answerNumber + 'なったかも！！'))
+      } else {
+        // false：正解の式が導出できなかった
+        // results.value.push(String(answerNumber + 'になりませんでしたぁ'))
+      }
     }
 
     // 四則演算記号の全バリエーションを配列へ設定
@@ -85,93 +85,92 @@ export default defineComponent({
       }
     }
 
-    // // 計算実行
-    // // 引数1：入力数字の配列
-    // const calculation = (inputNumberArray: Array<Big>) => {
-
-    //   // 試算用の計算順数値格納配列（ループ処理の中で要素の順番を入れ替える）
-    //   let calcNumberArray = inputNumberArray
+    // 計算実行
+    // 引数1：入力数字の配列
+    const calculation = (inputNumberArray: Array<Big>) => {
+      // 試算用の計算順数値格納配列（ループ処理の中で要素の順番を入れ替える）
+      let calcNumberArray = inputNumberArray
       
-    //   // 配列入れ替え時の数字退避用変数
-    //   let tmpNum: Big = Big(0) 
+      // 配列入れ替え時の数字退避用変数
+      // let tmpNum: Big = Big(0) 
 
-    //   // 入力数字から並び順のバリエーションを作成しながら試算
-    //   for( let i = 0; i < calcNumberArray.length; i++ ){
-    //     // 次のループ用に初期配列を作成（初回は処理不要だが･･･処理しても問題ない）
-    //     calcNumberArray = inputNumberArray
-    //     tmpNum = inputNumberArray[i]
-    //     calcNumberArray[i] = inputNumberArray[0]
-    //     calcNumberArray[0] = tmpNum
+      // 入力数字から並び順のバリエーションを作成しながら試算
+      for( let i = 0; i < calcNumberArray.length; i++ ){
+        // 次のループ用に初期配列を作成（初回は処理不要だが･･･処理しても問題ない）
+        calcNumberArray = inputNumberArray
+      //   tmpNum = inputNumberArray[i]
+      //   calcNumberArray[i] = inputNumberArray[0]
+      //   calcNumberArray[0] = tmpNum
 
-    //     // 先頭数字を固定し、2〜4桁目を順に入れ替えていく
-    //     for(let j = 0; j < 6; j++ ){
+      //   // 先頭数字を固定し、2〜4桁目を順に入れ替えていく
+      //   for(let j = 0; j < 6; j++ ){
 
-    //       // 初回
-    //       if( j === 0 ){
-    //         // 初回は並び替えずに処理させる
+      //     // 初回
+      //     if( j === 0 ){
+      //       // 初回は並び替えずに処理させる
 
-    //       // 奇数回目
-    //       }else if( j % 2 === 1 ){
-    //         // 2桁目と3桁目を入れ替える
-    //         calcNumberArray.splice(1, 2, calcNumberArray[2], calcNumberArray[1])
+      //     // 奇数回目
+      //     }else if( j % 2 === 1 ){
+      //       // 2桁目と3桁目を入れ替える
+      //       calcNumberArray.splice(1, 2, calcNumberArray[2], calcNumberArray[1])
             
-    //       // 偶数回目
-    //       }else{
-    //         // 3桁目と4桁目を入れ替える
-    //         calcNumberArray.splice(2, 2, calcNumberArray[3], calcNumberArray[2])
-    //       }
+      //     // 偶数回目
+      //     }else{
+      //       // 3桁目と4桁目を入れ替える
+      //       calcNumberArray.splice(2, 2, calcNumberArray[3], calcNumberArray[2])
+      //     }
 
-    //       // 四則演算記号のバリエーションを作成しながら試算
-    //       for(let i = 0; i < symbolVariations.value.length; i++ ){
-    //         // 四則演算記号のバリエーションを１つずつ取得し、カンマで区切って今回処理用の配列へ格納
-    //         let symbolVariation = symbolVariations.value[i].split(',')
+      //     // 四則演算記号のバリエーションを作成しながら試算
+      //     for(let i = 0; i < symbolVariations.value.length; i++ ){
+      //       // 四則演算記号のバリエーションを１つずつ取得し、カンマで区切って今回処理用の配列へ格納
+      //       let symbolVariation = symbolVariations.value[i].split(',')
 
-    //         // 途中計算結果格納用（初期値として先頭数字を設定）
-    //         let tmpCalcResult:Big = Big(calcNumberArray[0])
+      //       // 途中計算結果格納用（初期値として先頭数字を設定）
+      //       let tmpCalcResult:Big = Big(calcNumberArray[0])
 
-    //         // 計算式をクリア（初期値として先頭数字を設定）
-    //         formula.value = String(calcNumberArray[0])
+      //       // 計算式をクリア（初期値として先頭数字を設定）
+      //       formula.value = String(calcNumberArray[0])
 
-    //         // 数字・記号の前から順に計算
-    //         for(let j = 0; j < symbolVariation.length; j++){
-    //           // 記号によって計算用メソッドを振り分ける
-    //           switch (symbolVariation[j]) {
-    //             case '+':
-    //               tmpCalcResult=tmpCalcResult.add(calcNumberArray[j+1]) 
-    //               formula.value += ' + ' + (String)(calcNumberArray[j+1])
-    //               break;
-    //             case '-':
-    //               tmpCalcResult=tmpCalcResult.minus(calcNumberArray[j+1]) 
-    //               formula.value += ' - ' + (String)(calcNumberArray[j+1])
-    //               break;
-    //             case '*':
-    //               tmpCalcResult=tmpCalcResult.times(calcNumberArray[j+1]) 
-    //               formula.value += ' * ' + (String)(calcNumberArray[j+1])
-    //               break;
-    //             case '/':
-    //               // ゼロ割になる場合はスキップ
-    //               if(calcNumberArray[j+1].eq(0)){
-    //                 continue;
-    //               }else{
-    //                 tmpCalcResult=tmpCalcResult.div(calcNumberArray[j+1]) 
-    //                 formula.value += ' / ' + (String)(calcNumberArray[j+1])
-    //               }
-    //               break;
-    //             default:
-    //               break;
-    //           }
-    //         }
+      //       // 数字・記号の前から順に計算
+      //       for(let j = 0; j < symbolVariation.length; j++){
+      //         // 記号によって計算用メソッドを振り分ける
+      //         switch (symbolVariation[j]) {
+      //           case '+':
+      //             tmpCalcResult=tmpCalcResult.add(calcNumberArray[j+1]) 
+      //             formula.value += ' + ' + (String)(calcNumberArray[j+1])
+      //             break;
+      //           case '-':
+      //             tmpCalcResult=tmpCalcResult.minus(calcNumberArray[j+1]) 
+      //             formula.value += ' - ' + (String)(calcNumberArray[j+1])
+      //             break;
+      //           case '*':
+      //             tmpCalcResult=tmpCalcResult.times(calcNumberArray[j+1]) 
+      //             formula.value += ' * ' + (String)(calcNumberArray[j+1])
+      //             break;
+      //           case '/':
+      //             // ゼロ割になる場合はスキップ
+      //             if(calcNumberArray[j+1].eq(0)){
+      //               continue;
+      //             }else{
+      //               tmpCalcResult=tmpCalcResult.div(calcNumberArray[j+1]) 
+      //               formula.value += ' / ' + (String)(calcNumberArray[j+1])
+      //             }
+      //             break;
+      //           default:
+      //             break;
+      //         }
+      //       }
 
-    //         // 正解の値が導き出せたらtrueを返却して処理終了
-    //         if(tmpCalcResult.eq(answerNumber)){
-    //           return true
-    //         }
-    //       }
-    //     }
-    //   }
+      //       // 正解の値が導き出せたらtrueを返却して処理終了
+      //       if(tmpCalcResult.eq(answerNumber)){
+      //         return true
+      //       }
+      //     }
+      //   }
+      }
 
-    //   return false
-    // }      
+      return false
+    }      
 
     return {
       first,
